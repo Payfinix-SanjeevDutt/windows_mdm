edd695cba69c0c5582935fe0f951dd9cdeef547b
Payfinix-SanjeevDutt <109944450+Payfinix-SanjeevDutt@users.noreply.github.com>
2024-12-17 11:18:21 +0530

    updated discovery url

--- app/config.py
+++ app/config.py
@@ -5,13 +5,16 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'edad563bccd0896fdf438d700cf6a5f5a6d0f3334056237263c54e57280941be')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://suchet:suchet@localhost:5432/windows_mdm')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://suchet:suchet@localhost:5432/windows_mdm')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:windowsDB123@windows-mdm.cja4g8gggrln.us-east-2.rds.amazonaws.com:5432/windows-mdm')
+   
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AZURE_AD_TENANT_ID = os.getenv('AZURE_TENANT_ID')
     AZURE_AD_CLIENT_ID = os.getenv('AZURE_CLIENT_ID')
     AZURE_AD_CLIENT_SECRET = os.getenv('AZURE_CLIENT_SECRET')
     AZURE_GRAPH_API_URL = "https://graph.microsoft.com/v1.0"
    
-    print("AZURE_TENANT_ID:", os.getenv("AZURE_TENANT_ID"))
-    print("AZURE_CLIENT_ID:", os.getenv("AZURE_CLIENT_ID"))
-    print("AZURE_CLIENT_SECRET:", os.getenv("AZURE_CLIENT_SECRET"))
+    # print("AZURE_TENANT_ID:", os.getenv("AZURE_TENANT_ID"))
+    # print("AZURE_CLIENT_ID:", os.getenv("AZURE_CLIENT_ID"))
+    # print("AZURE_CLIENT_SECRET:", os.getenv("AZURE_CLIENT_SECRET"))
