d756ac0a1d2db83a899bcfc64fff0da3b157f85d
Payfinix-SanjeevDutt <109944450+Payfinix-SanjeevDutt@users.noreply.github.com>
2024-12-17 13:39:48 +0530

    added zappa_settings file

--- run.py
+++ run.py
@@ -11,4 +11,4 @@ app = create_app()
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, debug=True,ssl_context=("cert.pem", "key.pem"))
     port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
